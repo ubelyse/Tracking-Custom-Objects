a00ad30f67e2d9103db6ac80da5c7bb9f998bca6
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-20 18:04:05 +0200

    edit my daily activity

---
+++ app/src/main/java/com/example/toactivity/ChosenActivity.java
@@ -0,0 +1,30 @@
+package com.example.toactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ChosenActivity extends AppCompatActivity {
+
+    @BindView(R.id.locationTextView)  TextView mLocationTextView;
+    @BindView(R.id.listView)  ListView mListView;
+
+    private String[] MorningActivities=new String[]{"Stretch","Drink water","Exercise","Eat breakfast","Read a motivational quote","Listen to music","Do a mental puzzle","Get updated on the news","Plan your day","Pack a healthy snack for the day"};
+    private String[] MiddayActivities=new String[]{};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chosen);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, MorningActivities);
+
+    }
+}
---
+++ app/src/main/java/com/example/toactivity/ChosenActivityArrayAdapter.java
@@ -0,0 +1,10 @@
+package com.example.toactivity;
+
+import android.content.Context;
+
+public class ChosenActivityArrayAdapter {
+
+    private Context mContext;
+    private String[] mMorningActivities;
+    private String[] MiddayActivities;
+}
