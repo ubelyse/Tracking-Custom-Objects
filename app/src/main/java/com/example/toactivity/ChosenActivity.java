ef88c8f2ebc4f1cdc009194470a411491e9821c3
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-20 19:57:02 +0200

    added morning activities

--- app/src/main/java/com/example/toactivity/ChosenActivity.java
+++ app/src/main/java/com/example/toactivity/ChosenActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,33 +22,57 @@ public class ChosenActivity extends AppCompatActivity {
     @BindView(R.id.locationTextView)  TextView mLocationTextView;
     @BindView(R.id.listView)  ListView mListView;
     @BindView(R.id.sectionRadioGroup) RadioGroup mrdgrup;
+    RadioButton mbtn1,mbtn3;
 
     private String[] MorningActivities=new String[]{"Stretch","Drink water","Exercise","Eat breakfast","Read a motivational quote","Listen to music","Do a mental puzzle","Get updated on the news","Plan your day","Pack a healthy snack for the day"};
-    private String[] MiddayActivities=new String[]{};
+    private String[] MiddayActivities=new String[]{"Eat Lunch"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chosen);
+        LinearLayout linearLayout= findViewById(R.id.linearlayout1);
+        mbtn1=findViewById(R.id.radioButton1);
+        mbtn3=findViewById(R.id.radioButton3);
         ButterKnife.bind(this);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, MorningActivities);
         ArrayAdapter adapter1=new ArrayAdapter(this, android.R.layout.simple_list_item_1,MiddayActivities);
 
-        mListView.setAdapter(adapter);
+        if (mbtn1.isChecked()) {
+            mListView.setAdapter(adapter1);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(ChosenActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
+            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String restaurant = ((TextView) view).getText().toString();
+                    Toast.makeText(ChosenActivity.this, restaurant, Toast.LENGTH_LONG).show();
+                }
+            });
 
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
+            Intent intent = getIntent();
+            String location = intent.getStringExtra("location");
 
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+            mLocationTextView.setText("Here are all the restaurants near: " + location);
+        }
+
+        else if(mbtn3.isChecked()){
+            mListView.setAdapter(adapter);
+
+            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String restaurant = ((TextView) view).getText().toString();
+                    Toast.makeText(ChosenActivity.this, restaurant, Toast.LENGTH_LONG).show();
+                }
+            });
+
+            Intent intent = getIntent();
+            String location = intent.getStringExtra("location");
+
+            mLocationTextView.setText("Here are all the restaurants near: " + location);
+        }
+
 
     }
 }
