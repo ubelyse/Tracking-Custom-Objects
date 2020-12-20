2a3a168f1c4eff89983529429ca8d80b9b050113
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-20 17:50:49 +0200

    edit my daily activity

--- app/src/main/java/com/example/toactivity/DailyChoose.java
+++ app/src/main/java/com/example/toactivity/DailyChoose.java
@@ -3,11 +3,13 @@ package com.example.toactivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +20,7 @@ public class DailyChoose extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_daily_choose, container, false);
+        Button mSubmitButton = (Button) rootView.findViewById(R.id.submitButton);
         Button mCancelButton = (Button) rootView.findViewById(R.id.cancelButton);
         getDialog().setTitle("Simple Dialog");
 
@@ -25,6 +28,13 @@ public class DailyChoose extends DialogFragment {
             @Override
             public void onClick(View v) {
                 dismiss();
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
         return rootView;
