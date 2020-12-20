24d3e91d3707c58a29f5d13fa48d4fdeb0b13961
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-20 17:36:20 +0200

    added the Click Listener to Cancel Button

--- app/src/main/java/com/example/toactivity/DailyChoose.java
+++ app/src/main/java/com/example/toactivity/DailyChoose.java
@@ -7,13 +7,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DailyChoose extends DialogFragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_daily_choose, container, false);
+        Button mCancelButton = (Button) rootView.findViewById(R.id.cancelButton);
         getDialog().setTitle("Simple Dialog");
+
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
         return rootView;
+
+
     }
 }
