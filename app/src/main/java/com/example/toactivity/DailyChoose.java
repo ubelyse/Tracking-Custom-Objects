6ed149fb0a2edb2a73b841412f0a97a681c3e521
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-12-20 17:08:45 +0200

    added the main layout

---
+++ app/src/main/java/com/example/toactivity/DailyChoose.java
@@ -0,0 +1,14 @@
+package com.example.toactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyChoose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_choose);
+    }
+}
